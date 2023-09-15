f221f5648573b65118fb8fb331a4f642a684b232
Hong Hanh <hanhhahong98@gmail.com>
2023-09-15 17:04:12 +0700

    test push

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -3,7 +3,6 @@ import authReducer from './authen/authSlice';
 import registerReducer from './register/registerSlice'
 import { articleData } from "./article/articleReducer";
 
-
 export default combineReducers({
     auth: authReducer,
     register: registerReducer,
