bdeaa794d7772322266e0ecf8c2a3fe55fe590c1
Hong Hanh <hanhhahong98@g.gmail.com>
2023-11-22 16:30:34 +0700

    delete component

--- src/rootSaga.tsx
+++
@@ -1,8 +0,0 @@
-import { authSaga } from "./redux/authen/authSaga";
-import { all } from "redux-saga/effects";
-import { registerSaga } from "./redux/register/registerSaga";
-import articleSaga  from "./redux/article/articleSaga";
-
-export default function* rootSaga() {
-    yield all([authSaga(), registerSaga(), articleSaga()])
-}
